0a13ea007f5f3412873c39a527b5197fb52d8fc6
unknown <frankvilax@gmail.com>
2020-05-08 11:46:42 +0100

    Prettier

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,35 +1,51 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {}
 
-    @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
     }
+  }
 
-    @Get('/:id')
-    getTaskById(@Param('id') id: string) : Task {
-        return this.tasksService.getTaskById(id);
-    }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
 
-    @Post()
-    createTask(@Body() createTaskDto: CreateTaskDto): Task {
-        return this.tasksService.createTask(createTaskDto);
-    }
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
 
-    @Delete('/:id')
-    deleteTask(@Param('id') id: string) {
-        return this.tasksService.deleteTask(id);
-    }
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string) {
+    return this.tasksService.deleteTask(id);
+  }
 
-    @Patch('/:id/status')
-    updateTask(@Param('id') id: string, @Body('status') taskStatus: TaskStatus): Task {
-        return this.tasksService.updateTask(id, taskStatus);
-    }
+  @Patch('/:id/status')
+  updateTask(
+    @Param('id') id: string,
+    @Body('status') taskStatus: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTask(id, taskStatus);
+  }
 }
